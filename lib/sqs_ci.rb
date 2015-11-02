6bbc87c75ab9b9913a65c832a6ee7f7c6322a1db
nroose <nroose@gmail.com>
2015-11-01 17:40:35 -0800

    Trying to flush out the scope a little.  Not tested.  Still need to be able to access the queue without opening it up.

--- lib/sqs_ci.rb
+++ lib/sqs_ci.rb
@@ -2,33 +2,76 @@ require 'aws-sdk'
 
 class SqsCi
   def self.poll(config_file)
+    config config_file
+    poller.poll do |msg|
+      process msg
+    end
+  end
+
+  def self.poller
+    @poller ||= Aws::SQS::QueuePoller.new(q)
+  end
+
+  def self.read_config(config_file)
+    JSON.parse(File.read(config_file))
+  end
+
+  def self.config(config_file)
     case
     when (File.exists?(config_file) rescue nil)
-      config = JSON.parse(File.read(config_file))
+      config = read_config config_file
       q = config['q']
       s3_bucket = config['s3_bucket']
+      command = config['command']
     when (ENV['SQS_CI_Q'] && ENV['SQS_CI_S3_BUCKET'])
       q = ENV['SQS_CI_Q']
       s3_bucket = ENV['SQS_CI_S3_BUCKET']
+      command = ENV['SQS_CI_COMMAND']
     else
       fail 'config file parameter or env is required.'
     end
     puts "q: #{q}"
     puts "s3_bucket: #{s3_bucket}"
+    @q, @s3_bucket, command = q, s3_bucket, command
+  end
 
-    poller = Aws::SQS::QueuePoller.new(q)
+  def self.q
+    @q
+  end
 
-    poller.poll do |msg|
-      body = JSON.parse(msg['body'])
-      puts JSON.pretty_generate body
+  def self.s3_bucket
+    @s3_bucket
+  end
 
-      message = JSON.parse(body['Message'])
-      puts JSON.pretty_generate message
+  def self.command
+    @command
+  end
 
-      puts "name: #{message['repository']['name']}"
-      puts "repository: #{message['repository']['url']}"
-      puts "ref: #{message['ref']}"
+  def process(msg)
+    body = JSON.parse(msg['body'])
+    puts JSON.pretty_generate body
 
+    message = JSON.parse(body['Message'])
+    puts JSON.pretty_generate message
+
+    project = message['repository']['name']
+    puts "name: #{project}"
+
+    repo = message['repository']['url']
+    puts "repository: #{repo}"
+
+    ref = message['ref']
+    puts "ref: #{ref}"
+
+    output = `cd #{project} && #{command} #{ref}`
+
+    # set status for each test
+    # run the tests
+
+    save_logs(output, "#{dir}/log")
+  end
+
+  def save_logs(output, dir)
       s3 = Aws::S3::Resource.new(region:'us-west-2')
       puts s3.inspect
 
@@ -36,9 +79,6 @@ class SqsCi
       puts obj.inspect
 
       obj.put(body: 'test')
-
-      
-    end
   end
 end
 
