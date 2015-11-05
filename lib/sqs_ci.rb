6f365baba0d8c649ed6611733f7edac06270fe70
nroose <nroose@gmail.com>
2015-11-05 07:43:49 +0000

    Making it take command line options, trying to finish, still set up to use the ruby file...

--- lib/sqs_ci.rb
+++ lib/sqs_ci.rb
@@ -1,58 +1,73 @@
 require 'aws-sdk'
+require 'optparse'
 
 class SqsCi
-  def self.poll(config_file)
-    config config_file
+  class << self
+    attr_accessor(:q, :s3_bucket, :region, :command, :user)
+  end
+
+  def self.poll
+    config
     poller.poll do |msg|
       process msg
     end
   end
 
   def self.poller
+    ENV['AWS_REGION'] = region
     @poller ||= Aws::SQS::QueuePoller.new(q)
   end
 
-  def self.read_config(config_file)
-    JSON.parse(File.read(config_file))
-  end
+  def self.config
+    options = {}
+    OptionParser.new do |opts|
+      opts.banner = "Usage: [ruby] sqs_ci.rb [options] (*required)\n" +
+                    "       sqs_ci [options] (*required)\n" +
+                    "Uses sqs messages from GitHub to run tests."
 
-  def self.config(config_file)
-    case
-    when (File.exists?(config_file) rescue nil)
-      config = read_config config_file
-      q = config['q']
-      s3_bucket = config['s3_bucket']
-      command = config['command']
-    when (ENV['SQS_CI_Q'] && ENV['SQS_CI_S3_BUCKET'])
-      q = ENV['SQS_CI_Q']
-      s3_bucket = ENV['SQS_CI_S3_BUCKET']
-      command = ENV['SQS_CI_COMMAND']
-    else
-      fail 'config file parameter or env is required.'
+      opts.on("-qQUEUE", "--queue=QUEUE", "*Queue from GitHub") do |queue|
+        options[:q] = queue
+      end
+
+      opts.on("-bS3_BUCKET", "--s3-bucket=S3_BUCKET", "*S3 Bucket on AWS to which to copy the output and artifacts") do |s3|
+        options[:s3_bucket] = s3
+      end
+
+      opts.on("-rREGION", "--region=REGION", "*AWS Region for queue and s3") do |region|
+        options[:region] = region
+      end
+
+      opts.on("-cCOMMAND", "--command=COMMAND", "*Test Command to run") do |test_command|
+        options[:command] = test_command
+      end
+
+      opts.on("-uUSER", "--user=USER", "Only do tests for commits by this GitHub user") do |user|
+        options[:user] = user
+      end
+
+      opts.on("-h", "--help", "Prints this help") do
+        options[:h] = "h"
+        puts opts
+        exit
+      end
+    end.parse!
+
+    unless options[:h] || [:q, :s3_bucket, :region, :command].all? {|s| options.key? s}
+      raise OptionParser::MissingArgument, "Arguments -q, -s, -r, and -c are required. -h for help."
     end
-    puts "q: #{q}"
-    puts "s3_bucket: #{s3_bucket}"
-    @q, @s3_bucket, command = q, s3_bucket, command
+    self.q, self.s3_bucket, self.region, self.command, self.user = options.values_at(:q, :s3_bucket, :region, :command, :user)
   end
 
-  def self.q
-    @q
-  end
-
-  def self.s3_bucket
-    @s3_bucket
-  end
-
-  def self.command
-    @command
-  end
-
-  def process(msg)
+  def self.process(msg)
     body = JSON.parse(msg['body'])
     puts JSON.pretty_generate body
 
     message = JSON.parse(body['Message'])
     puts JSON.pretty_generate message
+
+    if user
+      raise "user #{message["head_commit"]["author"]["name"]} does not match #{user}" unless user == message["head_commit"]["author"]["name"]
+    end
 
     project = message['repository']['name']
     puts "name: #{project}"
@@ -63,23 +78,26 @@ class SqsCi
     ref = message['ref']
     puts "ref: #{ref}"
 
-    output = `cd #{project} && #{command} #{ref}`
+    commit_ref = message['head_commit']['id']
+
+    output = `cd #{project} && git checkout #{commit_ref} && git pull && #{command}`
+    puts output
 
     # set status for each test
     # run the tests
 
-    save_logs(output, "#{dir}/log")
+    save_logs(project, commit_ref, output, "#{project}/log")
   end
 
-  def save_logs(output, dir)
-      s3 = Aws::S3::Resource.new(region:'us-west-2')
-      puts s3.inspect
-
-      obj = s3.bucket(s3_bucket).object('test')
-      puts obj.inspect
-
-      obj.put(body: 'test')
+  def self.save_logs(project, commit_ref, output, dir)
+    s3 = Aws::S3::Resource.new(region:'us-west-2')
+    obj = s3.bucket(s3_bucket).object(commit_ref)
+    obj.put(body: output)
+    files = Dir.new dir
+    files.each do |file|
+      obj.upload_file(file)
+    end
   end
 end
 
-SqsCi.poll(ARGV[0])
+SqsCi.poll
